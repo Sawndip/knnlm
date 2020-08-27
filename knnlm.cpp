48bfdee9559711e933cfb4e487a2f95ab7c8949e
王一 WangYi <godspeed_china@yeah.net>
2020-08-27 18:15:43 +0800

    many updates

--- knnlm.cpp
+++ knnlm.cpp
@@ -15,14 +15,13 @@
 #include	<cmath>
 #include	<omp.h>
 using	namespace	std;
-const	unsigned	kmer=64;
 
 int	fd;
 struct	stat	sb;
 uint8_t	*data;
-uint64_t	data_size,	threads,	seed;
+uint64_t	data_size,	threads,	seed,	kmer;
 double	mean;
-uint8_t	w[kmer];
+uint8_t	w[1024];
 
 uint64_t	open_mmap(const	char	*F){
 	fd=open(F,	O_RDONLY);	if(fd<0)	return	0;
@@ -47,8 +46,7 @@ double	predict(uint8_t	*p,	double	*prob,	double	alpha){
 	double	pr[threads<<8]={};
 	#pragma omp parallel for
 	for(size_t	i=kmer-1;	i<data_size-1;	i++)	
-		if(data[i]==*p&&data+i!=p){
-		//if(*(uint16_t*)(data+i-1)==*(uint16_t*)(p-1)&&data+i!=p){
+		if(data[i]==*p&&(data[i-1]==*(p-1)||data[i-2]==*(p-2))&&data+i!=p){
 		double	s=(score(data+i,p)-mean)*alpha;
 		s=exp(s);		
 		pr[(omp_get_thread_num()<<8)+data[i+1]]+=s;
@@ -64,6 +62,7 @@ double	predict(uint8_t	*p,	double	*prob,	double	alpha){
 
 double	normalize(double	beta){
 	for(size_t	i=0;	i<kmer;	i++)	w[i]=powf(beta,kmer-1-i)*255;
+	seed=0;
 	double	x,	sx=0,	sxx=0,	sn=0x100000;
 	for(size_t	k=0;	k<0x100000;	k++){
 		size_t	i=wyrand(&seed)%(data_size-kmer-2)+kmer-1,j;
@@ -79,38 +78,38 @@ double	normalize(double	beta){
 void	document(void){
 	cerr<<"usage:	knnlm [options] [word1 word2 ...]\n";
 	cerr<<"\t-t:	text file=data.txt\n";
+	cerr<<"\t-k:	kmer=32\n";
 	cerr<<"\t-a:	sampling temperature=2.718\n";
-	cerr<<"\t-d:	kmer weight decay=auto\n";
-	cerr<<"\t-T:	number of threads=auto\n";
 	cerr<<"\t-b:	benckmark chars=0\n";
 	exit(0);
 }
 
 int	main(int	ac,	char	**av){
-	seed=wyhash64(time(NULL),0);
-	string	file="data.txt";	double	alpha=2,	beta=exp(-log(255)/kmer);	size_t	bench=0;	threads=omp_get_num_procs();
+	string	file="data.txt";	double	alpha=2.718;	size_t	bench=0;	kmer=32;	
 	if(ac<2)	document();
 	int	opt;
-	while((opt=getopt(ac,	av,	"t:a:d:T:b:"))>=0){
+	while((opt=getopt(ac,	av,	"t:k:a:b:"))>=0){
 		switch(opt){
 		case	't':	file=optarg;	break;
+		case	'k':	kmer=atoi(optarg);	break;
 		case	'a':	alpha=atof(optarg);	break;
-		case	'd':	beta=atof(optarg);	break;
-		case	'T':	threads=atoi(optarg);	break;
 		case	'b':	bench=atoi(optarg);	break;
 		default:	document();
 		}
 	}
+	threads=omp_get_num_procs();	omp_set_num_threads(threads);
 	if(!open_mmap(file.c_str()))	return	0;
-	alpha/=normalize(beta);
-	omp_set_num_threads(threads);
+	alpha/=normalize(exp(-log(255)/kmer));
 	if(!bench){
+		seed=wyhash64(time(NULL),0);
+		ofstream	fo("article.txt");
 		vector<uint8_t>	v;
 		for(size_t	i=0;	i<kmer;	i++)	v.push_back(wyrand(&seed)&255);
 		for(int	i=optind;	i<ac;	i++){
-			for(size_t	j=0;	j<strlen(av[i]);	j++) v.push_back(av[i][j]);
-			if(i+1<ac)	v.push_back(' ');
+			for(size_t	j=0;	j<strlen(av[i]);	j++){ v.push_back(av[i][j]); fo<<av[i][j];	}
+			if(i+1<ac){	v.push_back(' ');	fo<<' ';	}
 		}
+		fo.flush();
 		double	prob[256];
 		for(;;){
 			predict(v.data()+v.size()-1,prob,alpha);
@@ -122,9 +121,12 @@ int	main(int	ac,	char	**av){
 			}
 			putchar(c);	fflush(stdout);
 			v.push_back(c);
+			fo<<c;	fo.flush();
 		}
+		fo.close();
 	}
 	else{
+		seed=0;
 		cerr<<"benchmarking\n";
 		timeval	beg,	end;
 		gettimeofday(&beg,NULL);
